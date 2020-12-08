4831ddeaf498dcdd29c9c5cc84e9da635c273f38
kyspir <kasper9021@mail.ru>
2020-12-09 01:34:47 +0700

    so bad

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from config import connex_app
+
+connex_app.add_api('swagger.yml')
+@connex_app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    connex_app.run(host='0.0.0.0', port=5000, debug=True)
